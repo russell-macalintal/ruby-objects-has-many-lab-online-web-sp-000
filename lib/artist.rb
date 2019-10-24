e38e403bdbacaae1c362eaaeffda5ad402383dce
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-24 02:09:55 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end 
+
+  def songs
+    
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new
+  end
+  
+end 
